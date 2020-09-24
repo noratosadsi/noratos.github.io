af3d29d6661012d37ffdef641e99a17120d7ef4a
noratosadsi <32206581+noratosadsi@users.noreply.github.com>
2020-09-24 17:40:57 -0500

    pagina de pocho
    se cambia por otra pagina web para probar si puede funcionar como hosting

---
+++ js/inicio_sesion.js
@@ -0,0 +1,46 @@
+function Login(){ 
+var done=0; 
+var usuario=document.login.usuario.value; 
+var password=document.login.password.value; 
+
+
+if (usuario=="paola" && password=="3003538870") {
+	
+	window.location="GLOSARIO.HTML"; 
+}
+
+else if (usuario=="julian" && password=="3012783633") { 
+	window.location="GLOSARIO.HTML"; 
+}
+
+else if (usuario=="laura" && password=="3213873231") { 
+	window.location="GLOSARIO.HTML"; 
+}
+
+else if (usuario=="vanessa" && password=="3212571385") { 
+	window.location="GLOSARIO.HTML"; 
+}
+
+else if (usuario=="daniels" && password=="3147733150") { 
+	window.location="GLOSARIO.HTML"; 
+}
+
+else if (usuario=="tintinho" && password=="3132170617") { 
+	window.location="GLOSARIO.HTML"; 
+}
+else
+{
+	//ambos campos
+	alert("usuario y/o contraseña incorrecto");
+	//break;
+}
+
+
+
+
+
+
+
+} 
+
+//document.oncontextmenu = function(){return false} 
