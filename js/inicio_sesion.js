43d2a40a0f3e987e088daa5c50891588a941f3c6
noratosadsi <32206581+noratosadsi@users.noreply.github.com>
2020-09-24 18:21:00 -0500

    INICIO SESION
    VUELVO A DEJAR COMO ESTABA PARA HACER PRUEBAS

--- js/inicio_sesion.js
+++ js/inicio_sesion.js
@@ -6,27 +6,27 @@ var password=document.login.password.value;
 
 if (usuario=="paola" && password=="3003538870") {
 	
-	window.location="GLOSARIO.html"; 
+	window.location="GLOSARIO.HTML"; 
 }
 
 else if (usuario=="julian" && password=="3012783633") { 
-	window.location="GLOSARIO.html"; 
+	window.location="GLOSARIO.HTML"; 
 }
 
 else if (usuario=="laura" && password=="3213873231") { 
-	window.location="GLOSARIO.html"; 
+	window.location="GLOSARIO.HTML"; 
 }
 
 else if (usuario=="vanessa" && password=="3212571385") { 
-	window.location="GLOSARIO.html"; 
+	window.location="GLOSARIO.HTML"; 
 }
 
 else if (usuario=="daniels" && password=="3147733150") { 
-	window.location="GLOSARIO.html"; 
+	window.location="GLOSARIO.HTML"; 
 }
 
 else if (usuario=="tintinho" && password=="3132170617") { 
-	window.location="GLOSARIO.html"; 
+	window.location="GLOSARIO.HTML"; 
 }
 else
 {
