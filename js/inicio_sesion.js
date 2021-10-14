7bcfd3053068c6ebbdd78e7f515115b84025627f
noratosadsi <32206581+noratosadsi@users.noreply.github.com>
2021-10-14 18:04:12 -0500

    modificación glosario
    modificación del glosario

--- js/inicio_sesion.js
+++ js/inicio_sesion.js
@@ -3,35 +3,18 @@ var done=0;
 var usuario=document.login.usuario.value; 
 var password=document.login.password.value; 
 
-
-if (usuario=="paola" && password=="3003538870") {
-	
+if (usuario=="DAVV" && password=="PYME8") { 
 	window.location="GLOSARIO.html"; 
 }
 
-else if (usuario=="julian" && password=="3012783633") { 
+else if (usuario=="daniels" && password=="DANI2") { 
 	window.location="GLOSARIO.html"; 
 }
 
-else if (usuario=="laura" && password=="3213873231") { 
+else if (usuario=="Viviana" && password=="VIVI3") { 
 	window.location="GLOSARIO.html"; 
 }
-
-else if (usuario=="vanessa" && password=="3212571385") { 
-	window.location="GLOSARIO.html"; 
-}
-
-else if (usuario=="daniels" && password=="3147733150") { 
-	window.location="GLOSARIO.html"; 
-}
-
-else if (usuario=="tintinho" && password=="3132170617") { 
-	window.location="GLOSARIO.html"; 
-}
-else if (usuario=="jorgeburgos" && password=="colecciones") { 
-	window.location="GLOSARIO.html"; 
-}
-else if (usuario=="Viviana" && password=="3115182595") { 
+else if (usuario=="ALEJANDRA" && password=="ALEJA4") { 
 	window.location="GLOSARIO.html"; 
 }
 else if (usuario=="SIMSIM" && password=="SIMSIMELVAGO") { 
